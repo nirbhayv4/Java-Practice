df8f107c69b433d3fb1751e26201d43c985c2842
Nirbhay Verma <92136704+nirbhayv4@users.noreply.github.com>
2024-12-12 20:56:45 +0530

    Create SubOfTwoNum.java

---
+++ Basic/SubOfTwoNum.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class SubOfTwoNum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sub = a - b;
+        System.out.println(sub);
+    }
+}
