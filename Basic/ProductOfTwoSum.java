4df86ca8f023bc06dec8ead1cb12c8538fd156f3
Nirbhay Verma <nirbhayv4@gmail.com>
2024-12-13 23:56:52 +0530

    Created Product of Two Sum Program
    Product of two sum program in which taking input from user

---
+++ Basic/ProductOfTwoSum.java
@@ -0,0 +1,13 @@
+package Basic;
+
+import java.util.*;
+
+public class ProductOfTwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println(product);
+    }
+}
