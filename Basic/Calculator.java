3765f1b16cbd592dbe3890798a3f1f8580579fb8
Nirbhay Verma <nirbhayv4@gmail.com>
2024-12-17 22:26:02 +0530

    Created Basic Calculator Program

---
+++ Basic/Calculator.java
@@ -0,0 +1,38 @@
+package Basic;
+
+import java.util.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a : ");
+        int a = sc.nextInt();
+        System.out.println("Enter b : ");
+        int b = sc.nextInt();
+        System.out.println("Enter operator : ");
+        char operator = sc.next().charAt(0);
+        sc.close();
+
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+
+            default:
+                System.out.println("Wrong Operator");
+        }
+    }
+
+}
