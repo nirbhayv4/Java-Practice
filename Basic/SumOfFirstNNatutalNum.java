a6ecf52c97f150107a5193e13f8e53986082a671
Nirbhay Verma <nirbhayv4@gmail.com>
2024-12-18 18:15:48 +0530

    Created Sum of First N Natural Number using Loop

---
+++ Basic/SumOfFirstNNatutalNum.java
@@ -0,0 +1,19 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class SumOfFirstNNatutalNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        int sum = 0;
+
+        int i = 1;
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum is : " + sum);
+    }
+}
