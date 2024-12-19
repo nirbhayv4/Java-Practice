3fe044b5a3b8d1b9e3e96df5209d58e2a5913e92
Nirbhay Verma <nirbhayv4@gmail.com>
2024-12-19 22:11:07 +0530

    Reverse the given number

---
+++ Basic/ReverseTheNum.java
@@ -0,0 +1,20 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class ReverseTheNum {
+    public static void main(String[] args) {
+        System.out.println("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        int rev = 0;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            rev = (rev * 10) + lastDigit;
+            n = n / 10;
+        }
+        System.out.println(rev);
+    }
+}
