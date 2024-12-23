114425b90ea4fa6ddc356615d76328cc1956caec
Nirbhay Verma <nirbhayv4@gmail.com>
2024-12-23 23:38:26 +0530

    Created Hollow Rectangle Pattern

---
+++ Patterns/HollowRectanglePattern.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class HollowRectanglePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter number of columns: ");
+        int columns = scanner.nextInt();
+        scanner.close();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if (i == 1 || i == rows || j == 1 || j == columns) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
