8e4d2bbb845ca3da4136707f58b3beb312c5f331
Nirbhay Verma <nirbhayv4@gmail.com>
2024-12-24 23:56:08 +0530

    Fixed Scanner Warning

--- Basic/AreaOfCircle.java
+++ Basic/AreaOfCircle.java
@@ -6,6 +6,7 @@ public class AreaOfCircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float rad = sc.nextFloat();
+        sc.close();
         float area = 3.14f * rad * rad;
         System.out.println(area);
     }
